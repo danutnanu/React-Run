cabe0957de645b8df327d0386be586abde09916c
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-02-15 14:52:32 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          hello people
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
