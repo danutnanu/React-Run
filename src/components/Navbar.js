38ecc16f4c64e74bfd8d8211d9bbefd575b41c6e
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-02-18 17:52:55 +0000

    Add logo to Navbar function

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <div className="navbar-container">
+
+                    <div className="navbar-logo">
+                        RUN
+                    <i class="fa-solid fa-person-running" />
+                    </div>
+
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
