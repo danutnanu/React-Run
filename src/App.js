aa17dda5f36d78cd373ed8db7adda2a98f4ded52
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-02-19 06:37:43 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
-        <p>
-          hello people
-        </p>
-       
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        {/* <Switch>
+          <Route path='/' exact />
+        </Switch> */}
+      </Router>
+    </>
   );
 }
 
