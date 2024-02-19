fbd55fb7c511ac95b90975c358e914f79a7afa12
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-02-19 07:16:37 +0000

    Add link to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
+
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    
     return (
         <>
             <nav className='navbar'>
@@ -8,13 +16,43 @@ function Navbar() {
 
                     <div className="navbar-logo">
                         RUN
-                    <i class="fa-solid fa-person-running" />
+                        <i class="fa-solid fa-person-running" />
                     </div>
+
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/run2022' className='nav-links' onClick={closeMobileMenu}>
+                                2022
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/run2023'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                2023
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link
+                                to='/run2024'
+                                className='nav-links'
+                                onClick={closeMobileMenu}
+                            >
+                                2024
+                            </Link>
+                        </li>
+                    </ul>
 
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
