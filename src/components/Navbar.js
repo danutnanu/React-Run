6ce27bde80c92fc35635da6b83138250d8579e7a
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-02-28 06:56:21 +0000

    Add closeMobileMenu function to Logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ function Navbar() {
             <nav className='navbar'>
                 <div className="navbar-container">
 
-                    <div className="navbar-logo">
+                    <div className="navbar-logo" onClick={handleClick}>
                         RUN
                         <i class="fa-solid fa-person-running bg-danger" />
                     </div>
