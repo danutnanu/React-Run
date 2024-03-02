747b5418e2375354fb4e72ea9baca55f8960d0e7
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-02 10:54:12 +0000

    Add Bootstrap color

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,10 +7,10 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src="./videos/video-1.mp4" autoPlay loop muted />
-            <h1>"EVERYTHING</h1>
-            <p>YOU WANTED TO KNOW ABOUT</p>
-            <p>YOURSELF YOU CAN</p>
-            <p>LEARN IN</p>
+            <h1 className='text-warning'>"EVERYTHING</h1>
+            <p className='text-secondary'>YOU WANTED TO KNOW ABOUT</p>
+            <p className='text-secondary'>YOURSELF YOU CAN</p>
+            <p className='text-secondary'>LEARN IN</p>
             <p className='text-warning h2'>26.2 MILES"</p>
         </div>
     )
