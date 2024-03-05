e86a960a5e4e26e2773924e471882160a3953ada
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-05 14:53:17 +0000

    Add new path

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Run2022 from './components/pages/Run2022';
+import Run2023 from './components/pages/Run2023';
+import Run2024 from './components/pages/Run2024';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home} />
+          <Route path='/run2022' exact component={Run2022} />
+          <Route path='/run2023' exact component={Run2023} />
+          <Route path='/run2024' exact component={Run2024} />
         </Switch>
       </Router>
     </>
