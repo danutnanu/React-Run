fdd1e3ab0556c9370cd024c2721531ec628d16a1
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-05 14:58:07 +0000

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,12 +5,11 @@ import CardItem from './CardItem'
 function Cards() {
     return (
         <div className="cards">
-            <h1>Marathons aren't just about running.</h1>
-            <h1>They're about pushing your limits.</h1>
+            <h1 className='mb-5'>Marathons aren't just about running.</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
 
-                    <p className='mt-5 mb-5 text-center h1'>2022</p>
+                    <p className='m-5 text-center h1'>2022</p>
 
                     <ul className='cards__items'>
                         <CardItem
@@ -34,20 +33,25 @@ function Cards() {
                             src='images/logo.jpg'
                             text='First running in 13.1 miles'
                             label='Crazy Run'
-                            path='/run2022'
+                            path='/run2023'
                         />
                         <CardItem
                             src='images/run1.jpeg'
                             text='First running in 26.2 miles'
                             label='Crazy Run'
-                            path='/run2022'
+                            path='/run2023'
+                        />
+                        <CardItem
+                            src='images/run1.jpeg'
+                            text='First running in 26.2 miles'
+                            label='Crazy Run'
+                            path='/run2023'
                         />
                     </ul>
-
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Cards;
