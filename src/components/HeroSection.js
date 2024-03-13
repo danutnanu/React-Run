3777cfe68434d0ed65f4acc84687b9834d59c003
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-13 10:39:03 +0000

    Update video

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,11 +6,11 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src="./videos/video-1.mp4" autoPlay loop muted />
+      <video src="./videos/video1.mp4" autoPlay loop muted />
       <h1 className='text-warning'>"EVERYTHING</h1>
-      <p className='text-secondary'>YOU WANTED TO KNOW ABOUT</p>
-      <p className='text-secondary'>YOURSELF YOU CAN</p>
-      <p className='text-secondary'>LEARN IN</p>
+      <p className='text-info'>YOU WANTED TO KNOW ABOUT</p>
+      <p className='text-info'>YOURSELF YOU CAN</p>
+      <p className='text-info'>LEARN IN</p>
       <p className='text-warning h2'>26.2 MILES"</p>
     </div>
   );
