911513473537d6ae36d42c7bffe8e900caf83b86
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-14 07:47:27 +0000

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,11 +7,18 @@ function HeroSection() {
   return (
     <div className='hero-container'>
       <video src="./videos/video1.mp4" autoPlay loop muted />
-      <h1 className='text-warning'>"EVERYTHING</h1>
-      <p className='text-info'>YOU WANTED TO KNOW ABOUT</p>
-      <p className='text-info'>YOURSELF YOU CAN</p>
-      <p className='text-info'>LEARN IN</p>
-      <p className='text-warning h2'>26.2 MILES"</p>
+      <div className='hero-first m-5'>
+        <p className='h3'>Developed by: Danut Nanu</p>
+        <p className='h3'>Designed by: Danut Nanu</p>
+        <p className='h3'>Runner: Danut Nanu</p>
+      </div>
+
+      <div className='hero-second text-center m-5'>
+        <h1 className='text-warning h1'>"EVERYTHING</h1>
+        <p className='h1'>YOU WANTED TO KNOW</p>
+        <p className='h4'>ABOUT YOURSELF YOU CAN LEARN IN</p>
+        <p className='text-warning h2'>26.2 MILES"</p>
+      </div>
     </div>
   );
 }
