7afb369b18bc30afc565016dfc0ebe2501ebc985
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-17 11:00:42 +0000

    Add path for Story

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Story from './components/Story';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route exact path="/running-event/:id" component={Story} />
         </Routes>
       </Router>
     </>
