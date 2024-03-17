85a1175ed1e6f2fcc08cc372995b68a64b16b584
danut nanu <125583127+danutnanu@users.noreply.github.com>
2024-03-17 10:59:46 +0000

    Create Story files

---
+++ src/components/Story.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Story.css";
+
+function Story() {
+
+  return (
+    <>
+      <h1 className="text-primary text-center h1">Stories</h1>
+      <p>first story</p>
+    </>
+  );
+}
+
+export default Story;
